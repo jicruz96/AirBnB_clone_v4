5563ac47d133f233a1d96f8f358816409dafc217
jicruz96 <accounts@jicruz.com>
2020-09-26 02:35:01 +0000

    Make web_dynamic/static/scripts/1-hnbn.js comply with semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,24 +1,24 @@
 $().ready(function () {
-  $("INPUT").click(function () {
-    let checked = {};
-    let check = $("INPUT:checked");
-    let temp = $("INPUT:checked");
+  $('INPUT').click(function () {
+    const checked = {};
+    const check = $('INPUT:checked');
+    let temp = $('INPUT:checked');
     let i = 0;
 
     for (; i < check.length; i++) {
-      checked[temp.attr("data-id")] = temp.attr("data-name");
+      checked[temp.attr('data-id')] = temp.attr('data-name');
       temp = temp.slice(1);
     }
 
-    let names = "";
-    for (let key in checked) {
-      if (names === "") {
+    let names = '';
+    for (const key in checked) {
+      if (names === '') {
         names = names + checked[key];
       } else {
         names = names + ', ' + checked[key];
       }
     }
 
-    $("DIV.amenities H4").text(names);
+    $('DIV.amenities H4').text(names);
   });
 });
