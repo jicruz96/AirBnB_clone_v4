8c2361837877f0906e8c94c200a57ca74444f3c2
jicruz96 <accounts@jicruz.com>
2020-09-26 02:32:05 +0000

    Initial commit of web_dynamix/statix/scripts/2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+jQuery.getJSON('http://192.168.33.10:5001/api/v1/status/', function (data, textStatus) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  }
+});
+
+$().ready(function () {
+  $('INPUT').click(function () {
+    const checked = {};
+    const check = $('INPUT:checked');
+    let temp = $('INPUT:checked');
+    let i = 0;
+
+    for (; i < check.length; i++) {
+      checked[temp.attr('data-id')] = temp.attr('data-name');
+      temp = temp.slice(1);
+    }
+
+    let names = '';
+    for (const key in checked) {
+      if (names === '') {
+        names = names + checked[key];
+      } else {
+        names = names + ', ' + checked[key];
+      }
+    }
+
+    $('DIV.amenities H4').text(names);
+  });
+});
